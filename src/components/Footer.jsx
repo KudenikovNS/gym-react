ad79b29d515d471a91350180ae6f2f1f658152d0
Nikita Kudenikov <KudenikovNS@yandex.ru>
2024-05-13 09:12:15 +0200

    Add Footer section

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,9 +1,27 @@
-import React from 'react'
+import React from "react";
+import { FaCopyright } from "react-icons/fa";
+import { Link } from "react-scroll";
+import { FaArrowUp } from "react-icons/fa6";
 
 const Footer = () => {
   return (
-    <div>Footer section</div>
-  )
-}
+    <>
+      <section className="bg-black w-full text-white p-3 flex justify-center items-center gap-4">
+        <FaCopyright className="size-6 text-white" />
+        <h1 className="text-white text-center font-semibold">
+          Nikita Kudenikov
+        </h1>
+      </section>
+      <div
+        id="icon-box"
+        className="bg-red-500 text-black p-3 rounded-full hover:bg-white cursor-pointer fixed lg:bottom-6 right-6 bottom-6"
+      >
+        <Link to="hero" spy={true} offset={-100} smooth={true}>
+          <FaArrowUp className="w-[35px] h-[35px]"/>
+        </Link>
+      </div>
+    </>
+  );
+};
 
-export default Footer
+export default Footer;
